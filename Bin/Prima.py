6aa761258dafd64dd02d19129a6e30c5a843b65f
Noor Azam <noor@rad.net.id>
2018-08-18 20:14:23 +0700

    menambah (int) untuk kompatibilitas dengan Windows.

--- Bin/Prima.py
+++ Bin/Prima.py
@@ -1,7 +1,7 @@
 def mainprog(awal, akhir):
 	for i in range(awal, akhir+1):
 		prima = True
-		for j in range(2, (i/2)+1):
+		for j in range(2, int(i/2)+1):
 			if i % j == 0:
 				prima = False
 				break
